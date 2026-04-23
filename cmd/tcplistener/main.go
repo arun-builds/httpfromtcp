ab95167c8320f5ec8dda59073ce07ad5c2f771a1
arunprajapati-arch <arunprajapati.mail@gmail.com>
2026-04-23 07:52:30 +0530

    internal/tcplistener cleanup

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,57 +1,22 @@
 package main
 
 import (
-	"bytes"
+
 	"fmt"
-	"io"
+
 	"log"
 	"net"
+
+	"github.com/arun-builds/httpfromtcp/internal/request"
 )
-
-func GetLinesFromReader(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-
-		data := make([]byte, 8)
-		str := ""
-
-		for {
-
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-			data := data[:n]
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i+1:]
-				out <- str
-				str = ""
-			}
-
-			str += string(data)
-		}
-
-		if len(str) != 0 {
-			out <- str
-		}
-
-	}()
-
-	return out
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("error", err)
 	}
 
-	defer listener.Close() 
 	for {
 		conn, err := listener.Accept()
 
@@ -59,13 +24,16 @@ func main() {
 			log.Fatal("error", err)
 		}
 
-		fmt.Println("A connection has been accepted")
+		r, err := request.RequestFromReader(conn)
 
-		for line := range GetLinesFromReader(conn) {
-			fmt.Printf("read: %s\n", line)
+		if err != nil {
+			log.Fatal("error", err)
 		}
 
-		fmt.Println("Connection closed")
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 
 }
