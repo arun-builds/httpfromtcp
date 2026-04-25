c452a0223c03b5bc0d02c55e42633010591cc5cc
arunprajapati-arch <arunprajapati.mail@gmail.com>
2026-04-25 16:13:57 +0530

    add handlers

---
+++ cmd/httpserver/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/arun-builds/httpfromtcp/internal/request"
+	"github.com/arun-builds/httpfromtcp/internal/response"
+	"github.com/arun-builds/httpfromtcp/internal/server"
+)
+
+const port = 42069
+
+func main() {
+	s, err := server.Serve(port, func(w io.Writer, req *request.Request)*server.HandleError{
+		if req.RequestLine.RequestTarget == "/yourproblem"{
+			return &server.HandleError{
+				StatusCode: response.StatusBadRequest,
+				Message: "Your problem is not my problem\n",
+			}
+		}else if req.RequestLine.RequestTarget == "/myproblem"{
+			return &server.HandleError{
+				StatusCode: response.StatusInternalServerError,
+				Message: "Woopsie, my bad\n",
+			}
+		}else {
+			w.Write([]byte("All good, frfr\n"))
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+	defer s.Close()
+	log.Println("Server started on port", port)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	log.Println("Server gracefully stopped")
+}
